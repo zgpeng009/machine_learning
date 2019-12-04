7e72146639bf26cd68018ee6ecbe67f8138f8666
rainf-pzg <844202126@qq.com>
2019-12-04 18:20:57 +0800

    init

--- demo1.py
+++ demo1.py
@@ -4,9 +4,25 @@ import numpy as np
 import pandas as pd
 import sklearn
 
+
+def prepare_country_stats(p1, p2):
+    _a = {'GDP per capita': [], 'Life satisfaction': []}
+    _p1 = p1[['Country', 'Value']]
+    _p2 = p2[['Country', '2015']]
+    c = pd.merge(_p1, _p2, on='Country', how='left')
+    c.to_csv('./dataset/results.csv')
+    _a['GDP per capita'] = c['Value'].as_matrix()
+    _a['Life satisfaction'] = c.as_matrix(columns=c.columns[1:2])
+    return _a
+
+
 #  Load the data
-oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
-gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values="n/a")
+oecd_bli = pd.read_csv("./dataset/oecd_bli_2015.csv", thousands=',')
+_gdp_per_capita = pd.read_csv("./dataset/gdp_per_capita.xls", thousands=',', delimiter='\t', encoding='latin1',
+                             na_values="n/a")
+df = _gdp_per_capita
+gdp_per_capita = df.loc[df['Estimates Start After'].isin(['2015'])]
+gdp_per_capita.to_csv('./dataset/results1.csv')
 
 #  Prepare the data
 country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
